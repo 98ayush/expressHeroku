b2f8e5c650c490f9f1ce1ff36069af86193d2dd7
ayush_98 <ayushagrawal17111998@gmail.com>
2020-09-15 22:48:58 +0530

    second

--- expserver.js
+++ expserver.js
@@ -3,10 +3,10 @@ const bodyParser=require('body-parser');
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/calculator',function(req,res){
+app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
-app.post('/calculator',function(req,res){
+app.post('/',function(req,res){
     let n1=Number(req.body.v1);
     let n2=Number(req.body.v2);
     let sum=n1+n2;
